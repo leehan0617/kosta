ef5f011196dd5680bee3d15525b6dcd9dd6e6238
unknown <leehan0617@gmail.com>
2015-02-13 15:07:57 +0900

    150213

--- pro01/src/ch04/Ex39.java
+++ pro01/src/ch04/Ex39.java
@@ -1,5 +1,11 @@
 package ch04;
-
+/*
+ * 메모리 구조
+ * constant 영역  : 전역변수 , static 변수 (프로그램 시작될때 생성되고 종료될때 없어진다.)
+ * heap     영역  : new(class,배열)	   (가비지컬렉션)
+ * stack    영역  : 지역변수 (블럭단위로 생성되고 없어진다.)
+ * register 영역  : 우선순위높은 변수(잘안쓰임)
+ */
 public class Ex39 {
 	public static void main(String[] args) {
 		int x=10;
---
+++ pro01/src/ch04/Ex40.java
@@ -0,0 +1,22 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Ex40 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String name="";
+		int kor=0,eng=0,mat=0;
+		
+		System.out.print("name:");
+		name =scan.next();
+		System.out.print("kor:");
+		kor=scan.nextInt();
+		System.out.print("eng:");
+		eng=scan.nextInt();
+		System.out.print("mat");
+		mat=scan.nextInt();
+		
+	}
+
+}
