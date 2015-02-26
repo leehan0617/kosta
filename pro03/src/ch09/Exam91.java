a62151bdcbdd284869fa48151a8cff1e55ff3c20
unknown <leehan0617@gmail.com>
2015-02-26 17:14:44 +0900

    150226

--- pro03/src/ch09/Exam91.java
+++ pro03/src/ch09/Exam91.java
@@ -1,5 +1,7 @@
 package ch09;
 
+import java.util.Date;
+
 /*
  * java.lang.Object
  * 1)모든 클래스들은 Object 클래스 상속받는다.
@@ -9,8 +11,37 @@ package ch09;
  * 4)메소드 :hashCode,wait,notify,notifyAll,toString,equal
  * 			clone,finalize,getClass
  */
+
+/*
+ *  toString: 문자열로 반환
+ */
+
+class AA{
+	private int x,y;
+	
+	public AA(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	public String toString(){
+		String str="x:"+x+"y:"+y;
+		return str;
+	}
+}
+
 public class Exam91 {
 	public static void main(String[] args) {
+		Integer a=new Integer("10");
+		System.out.println(++a);
 		
+		String b=a.toString();
+		System.out.println("b:" + b);
+		
+		Date d=new Date();
+		System.out.println(d.toString());
+		
+		AA aa=new AA(10,20);
+		System.out.println(aa.toString());
 	}
 }
